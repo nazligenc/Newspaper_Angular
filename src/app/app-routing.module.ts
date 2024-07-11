05b8fd61a8a6eddc0b6b4aaf420435d55b7d7fb1
nazligenc <nazli.gencx@gmail.com>
2024-07-11 15:31:33 +0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import{HomeComponent} from "./home/home.component";
+import {CategoriesComponent} from "./categories/categories.component";
+import {ContactComponent} from "./contact/contact.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:HomeComponent},
+  {path:"home",component:HomeComponent},
+  {path:"categories",component:CategoriesComponent},
+  {path:"politics",component:CategoriesComponent},
+  {path:"business",component:CategoriesComponent},
+  {path:"health",component:CategoriesComponent},
+  {path:"design",component:CategoriesComponent},
+  {path:"sport",component:CategoriesComponent},
+  {path:"contact",component:ContactComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SliderComponent } from './slider/slider.component';
 import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SliderComponent,
-    HomeComponent
+    HomeComponent,
+    HeaderComponent,
+    CategoriesComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
