7c26ba36f490b53b9775e9b097a66ec7c24905cd
nazligenc <nazli.gencx@gmail.com>
2024-07-23 16:13:50 +0300

    api update

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,8 @@ import { NewsServiceService } from "../news-service.service";
   templateUrl: './home.component.html'
 })
 export class HomeComponent implements OnInit {
+  politicsNews: any[] = [];
+  businessNews: any[] = [];
   hdata: any[] = [];
   currentPage: number = 1;
   pageSize: number = 25; // Sayfa başına haber sayısı
@@ -16,6 +18,8 @@ export class HomeComponent implements OnInit {
 
   ngOnInit(): void {
     this.fetchData(this.currentPage, this.pageSize);
+    this.fetchPoliticsNews(this.currentPage, this.pageSize);
+    this.fetchBusinessNews(this.currentPage, this.pageSize);
   }
 
   fetchData(page: number, pageSize: number): void {
@@ -31,16 +35,46 @@ export class HomeComponent implements OnInit {
       }
     });
   }
+  fetchPoliticsNews(page: number, pageSize: number): void {
+    this.serviceComponent.getPoliticsNews(page, pageSize).subscribe({
+      next: (data: any) => {
+        this.politicsNews = data.articles;
+        console.log(this.politicsNews);
+      },
+      error: (error: any) => {
+        console.error('Error fetching politics news', error);
+      },
+      complete: () => {
+        console.log('API politics news fetch completed.');
+      }
+    });
+  }
+  fetchBusinessNews(page: number, pageSize: number): void {
+    this.serviceComponent.getBusinessNews(page, pageSize).subscribe({
+      next: (data: any) => {
+        this.businessNews = data.articles;
+        console.log(this.businessNews);
+      },
+      error: (error: any) => {
+        console.error('Error fetching business news', error);
+      },
+      complete: () => {
+        console.log('API business news fetch completed.');
+      }
+    });
+  }
 
   nextPage(): void {
     this.currentPage++;
     this.fetchData(this.currentPage, this.pageSize);
+
   }
 
   prevPage(): void {
     if (this.currentPage > 1) {
       this.currentPage--;
       this.fetchData(this.currentPage, this.pageSize);
+
     }
   }
   goToDetail(url: string) {
--- src/app/news-service.service.ts
+++ src/app/news-service.service.ts
@@ -17,6 +17,44 @@ export class NewsServiceService{
     const url = `${this.url}&page=${page}&pageSize=${pageSize}`;
     return this.http.get<any[]>(url);
   }
+  getPoliticsNews(page: number, pageSize: number): Observable<any[]> {
+    const url = `${this.url}&category=politics&page=${page}&pageSize=${pageSize}`;
+    return this.http.get<any[]>(url);
+  }
+  getBusinessNews(page: number, pageSize: number): Observable<any[]> {
+    const url = `${this.url}&category=business&page=${page}&pageSize=${pageSize}`;
+    return this.http.get<any[]>(url);
+  }
+
+  // Entertainment News
+  getEntertainmentNews(page: number, pageSize: number): Observable<any[]> {
+    const url = `${this.url}&category=entertainment&page=${page}&pageSize=${pageSize}`;
+    return this.http.get<any[]>(url);
+  }
+
+  // Health News
+  getHealthNews(page: number, pageSize: number): Observable<any[]> {
+    const url = `${this.url}&category=health&page=${page}&pageSize=${pageSize}`;
+    return this.http.get<any[]>(url);
+  }
+
+  // Science News
+  getScienceNews(page: number, pageSize: number): Observable<any[]> {
+    const url = `${this.url}&category=science&page=${page}&pageSize=${pageSize}`;
+    return this.http.get<any[]>(url);
+  }
+
+  // Sports News
+  getSportsNews(page: number, pageSize: number): Observable<any[]> {
+    const url = `${this.url}&category=sports&page=${page}&pageSize=${pageSize}`;
+    return this.http.get<any[]>(url);
+  }
+
+  // Technology News
+  getTechnologyNews(page: number, pageSize: number): Observable<any[]> {
+    const url = `${this.url}&category=technology&page=${page}&pageSize=${pageSize}`;
+    return this.http.get<any[]>(url);
+  }
 
 
 }
